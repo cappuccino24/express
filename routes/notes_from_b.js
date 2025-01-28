5f27846d52852cfc8222bc013d1546ea0fbac8aa
cappuccino24 <faito5353@gmail.com>
2025-01-28 21:58:31 +0900

    a

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
 
+// corsミドルウェアを使用
+router.use(cors());
+
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
@@ -13,9 +16,8 @@ router.get('/', async (req, res) => {
 	const database = client.db('notes');
 	const notes = database.collection('notes');
 
-	// idが１のドキュメントを取得
-	const query = { id: 1 };
-	const note = await notes.findOne(query);
+	// すべてのドキュメントを取得
+	const note =await notes.find({}).toArray();
 
 	res.json(note);
 })
